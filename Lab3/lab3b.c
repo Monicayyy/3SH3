694c36eaa445a2f37d294876b052232d01d040c5
waic6 <waic6@mcmaster.ca>
2026-03-09 04:30:44 +0000

    updated part b

--- Lab3/lab3b.c
+++ Lab3/lab3b.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <sys/mman.h>   /* mmap(), munmap() */
-#include <string.h>     /* memcpy() */
-#include <fcntl.h>      /* open() */
-#include <stdlib.h>
-#include <unistd.h>     /* close() */
+#include <sys/mman.h>   /*For mmap() function*/
+#include <string.h>     /*For memcpy() function*/
+#include <fcntl.h>      /*For file descriptors: open()*/
+#include <stdlib.h>      /*For file descriptors*/
 
-#define INT_SIZE 4
-#define INT_COUNT 10
+#define INT_SIZE 4 // Size of integer in bytes
+#define INT_COUNT 10 
 #define MEMORY_SIZE (INT_COUNT * INT_SIZE)
 
 int intArray[INT_COUNT];
@@ -18,20 +17,11 @@ int main(void)
     int i;
     int sum = 0;
 
-    /* Open the binary file for reading */
+    /* Open the binary file for reading*/
     mmapfile_fd = open("numbers.bin", O_RDONLY);
-    if (mmapfile_fd < 0) {
-        perror("open");
-        return 1;
-    }
-
-    /* Memory-map the file */
+ 
+    /* Memory-map the file*/
     mmapfptr = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, mmapfile_fd, 0);
-    if (mmapfptr == MAP_FAILED) {
-        perror("mmap");
-        close(mmapfile_fd);
-        return 1;
-    }
 
     /* Copy one integer (4 bytes) at a time into intArray */
     for (i = 0; i < INT_COUNT; i++) {
@@ -39,14 +29,7 @@ int main(void)
     }
 
     /* Unmap the file */
-    if (munmap(mmapfptr, MEMORY_SIZE) == -1) {
-        perror("munmap");
-        close(mmapfile_fd);
-        return 1;
-    }
-
-    /* Close the file descriptor */
-    close(mmapfile_fd);
+    munmap(mmapfptr, MEMORY_SIZE);
 
     /* Add all integers in the array */
     for (i = 0; i < INT_COUNT; i++) {
